fe9c7c4af994f95a541ab5490774b703a31206d8
Justpie8 <navaltajohan@gmail.com>
2023-05-12 20:19:28 +0545

    To traverse file system using history.txt as data(file path)

---
+++ dir.sh
@@ -0,0 +1,20 @@
+#uses dmenu to traverse file 
+#uses history.txt as a file to traverse 
+#keybinded with MOD+s in dwm
+
+#!/bin/sh
+#
+hello=$( awk '{print}' ~/script/history.txt | dmenu -i -fn 20 -l 30 )
+echo $hello
+if [ "$hello" =  "" ]
+then
+    exit
+fi
+xdotool key Alt+d
+sleep 1s
+xdotool type "st"
+xdotool key Return
+sleep 1.5s
+xdotool type "cd "
+xdotool type "$hello"
+xdotool key Return
