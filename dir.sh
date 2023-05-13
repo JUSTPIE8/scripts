0521ed87d9bcc05629eb45fed6094aba94df6598
Justpie8 <navaltajohan@gmail.com>
2023-05-13 12:23:41 +0545

    auto create history file

--- dir.sh
+++ dir.sh
@@ -1,7 +1,7 @@
 #uses dmenu to traverse file 
 #uses history.txt as a file to traverse 
 #keybinded with MOD+s in dwm
-
+#one small problem is it determines file with '.' so if some file doesnot have  a dot it tries to cd it 
 run(){
     echo "$1"
     echo "$2"
@@ -19,18 +19,36 @@ exit
 
 #!/bin/sh
 #
-hello=$( awk '{print}' ~/script/history.txt | dmenu -i -fn 20 -l 30 )
-echo $hello
-if [ "$hello" =  "" ]
+#
+#create history file if not exists 
+#Only Desktop and Downloads are used since they are mainly i use
+if [ ! -f ~/script/history.txt ]; then
+
+touch ~/script/history.txt
+cd ~
+ ls -d -1 "$PWD/"* >~/script/history.txt
+cd ~/Desktop
+ ls -d -1 "$PWD/"* >>~/script/history.txt
+ ls -d -1 "$PWD/"*/* >>~/script/history.txt
+
+cd ~/Downloads
+ls -d -1 "$PWD/"* >>~/script/history.txt
+fi
+
+#dest u want to go 
+dest=$( awk '{print}' ~/script/history.txt | dmenu -i -fn 20 -l 30 )
+#echo $dest
+if [ "$dest" =  "" ]
 then
     exit
 fi
 dot="."
-case $hello in
+case $dest in
     *"$dot"*)
         cmd="xdg-open"
-run "$cmd" "$hello"
+run "$cmd" "$dest"
 esac
 
 cmd="cd"
-run "$cmd" "$hello"
+run "$cmd" "$dest"
+
