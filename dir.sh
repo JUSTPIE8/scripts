7974b7e2bd801002f37580ff275b5c8c0a1a8dc7
Justpie8 <navaltajohan@gmail.com>
2023-05-13 07:33:21 +0545

    updated dir.sh to work using case

--- dir.sh
+++ dir.sh
@@ -2,6 +2,21 @@
 #uses history.txt as a file to traverse 
 #keybinded with MOD+s in dwm
 
+run(){
+    echo "$1"
+    echo "$2"
+  xdotool key Alt+d
+sleep 1s
+xdotool type "st"
+xdotool key Return
+sleep 1.5s
+xdotool type "$1 "
+xdotool type "$2"
+xdotool key Return
+exit
+}
+
+
 #!/bin/sh
 #
 hello=$( awk '{print}' ~/script/history.txt | dmenu -i -fn 20 -l 30 )
@@ -10,25 +25,12 @@ if [ "$hello" =  "" ]
 then
     exit
 fi
+dot="."
+case $hello in
+    *"$dot"*)
+        cmd="xdg-open"
+run "$cmd" "$hello"
+esac
 
-if [[ -d $hello ]]; then
-xdotool key Alt+d
-sleep 1s
-xdotool type "st"
-xdotool key Return
-sleep 1.5s
-xdotool type "cd "
-xdotool type "$hello"
-xdotool key Return
-
-else
-    xdotool key Alt+d
-sleep 1s
-xdotool type "st"
-xdotool key Return
-sleep 1.5s
-xdotool type "xdg-open "
-xdotool type "$hello"
-xdotool key Return
-
-fi
+cmd="cd"
+run "$cmd" "$hello"
